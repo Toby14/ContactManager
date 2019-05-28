62f45f5988c340bd43caa0c135fce30e8cb4d85b
Toby <mtobytran@live.com>
2019-05-28 17:09:27 -0400

    added onclickListener for reg_button

--- app/src/main/java/com/example/contactmanager/RegisterActivity.java
+++ app/src/main/java/com/example/contactmanager/RegisterActivity.java
@@ -1,13 +1,30 @@
 package com.example.contactmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button mRegister;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mRegister = findViewById(R.id.reg_button);
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
